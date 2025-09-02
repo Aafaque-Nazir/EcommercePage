e538c40a208056959d1910fade51e983b6b3a239
Aafaque Nazir <aafaquenazir@gmail.com>
2025-09-02 22:12:02 +0530

    ,.,.

--- src/app/lib/products.js
+++
@@ -1,7 +0,0 @@
-const products = [
-  { id: "1", name: "Nike Shoes", price: 4999, image: "/shoe.jpg" },
-  { id: "2", name: "Smart Watch", price: 7999, image: "/watch.jpg" },
-  { id: "3", name: "Headphones", price: 2999, image: "/headphone.jpg" },
-]
-
-export default products
